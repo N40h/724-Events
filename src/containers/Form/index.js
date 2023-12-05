9a58ee4ae425286d12a68947def56419f792e93d
Naoh <27915369+N40h@users.noreply.github.com>
2023-12-05 14:28:07 +0100

    Fix display issues for the slides, fixed the filter of events, fixed issues regardless the form

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -1,66 +1,96 @@
-import { useCallback, useState } from "react";
-import PropTypes from "prop-types";
-import Field, { FIELD_TYPES } from "../../components/Field";
-import Select from "../../components/Select";
-import Button, { BUTTON_TYPES } from "../../components/Button";
+import { useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
+import Field, { FIELD_TYPES } from '../../components/Field';
+import Select from '../../components/Select';
+import Button, { BUTTON_TYPES } from '../../components/Button';
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+const mockContactApi = () =>
+	new Promise((resolve) => {
+		setTimeout(resolve, 1000);
+	});
 
 const Form = ({ onSuccess, onError }) => {
-  const [sending, setSending] = useState(false);
-  const sendContact = useCallback(
-    async (evt) => {
-      evt.preventDefault();
-      setSending(true);
-      // We try to call mockContactApi
-      try {
-        await mockContactApi();
-        setSending(false);
-      } catch (err) {
-        setSending(false);
-        onError(err);
-      }
-    },
-    [onSuccess, onError]
-  );
-  return (
-    <form onSubmit={sendContact}>
-      <div className="row">
-        <div className="col">
-          <Field placeholder="" label="Nom" />
-          <Field placeholder="" label="Prénom" />
-          <Select
-            selection={["Personel", "Entreprise"]}
-            onChange={() => null}
-            label="Personel / Entreprise"
-            type="large"
-            titleEmpty
-          />
-          <Field placeholder="" label="Email" />
-          <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
-            {sending ? "En cours" : "Envoyer"}
-          </Button>
-        </div>
-        <div className="col">
-          <Field
-            placeholder="message"
-            label="Message"
-            type={FIELD_TYPES.TEXTAREA}
-          />
-        </div>
-      </div>
-    </form>
-  );
+	const [sending, setSending] = useState(false);
+	const sendContact = useCallback(
+		async (evt) => {
+			evt.preventDefault();
+
+			const form = evt.target;
+			const fields = form.querySelectorAll('input[name], textarea[name]');
+			const selectionField = form.querySelector('.Select');
+			const selectionValue = fields[2].value;
+			let isFormValid = true;
+
+			fields.forEach((field) => {
+				if (!field.value) {
+					isFormValid = false;
+					field.classList.add('error');
+				} else {
+					field.classList.remove('error');
+				}
+			});
+
+			if (!selectionValue) {
+				selectionField.classList.add('error');
+			} else {
+				selectionField.classList.remove('error');
+			}
+
+			if (!isFormValid) {
+				return;
+			}
+
+			setSending(true);
+			// We try to call mockContactApi
+			try {
+				await mockContactApi();
+				setSending(false);
+				onSuccess();
+			} catch (err) {
+				setSending(false);
+				onError(err);
+			}
+		},
+		[onSuccess, onError]
+	);
+	return (
+		<form onSubmit={sendContact}>
+			<div className="row">
+				<div className="col">
+					<Field placeholder="" label="Nom" />
+					<Field placeholder="" label="Prénom" />
+					<Select
+						selection={['Personel', 'Entreprise']}
+						onChange={() => null}
+						label="Personel / Entreprise"
+						type="large"
+						titleEmpty
+					/>
+					<Field placeholder="" label="Email" />
+					<Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
+						{sending ? 'En cours' : 'Envoyer'}
+					</Button>
+				</div>
+				<div className="col">
+					<Field
+						placeholder="message"
+						label="Message"
+						type={FIELD_TYPES.TEXTAREA}
+					/>
+				</div>
+			</div>
+		</form>
+	);
 };
 
 Form.propTypes = {
-  onError: PropTypes.func,
-  onSuccess: PropTypes.func,
-}
+	onError: PropTypes.func,
+	onSuccess: PropTypes.func,
+};
 
 Form.defaultProps = {
-  onError: () => null,
-  onSuccess: () => null,
-}
+	onError: () => null,
+	onSuccess: () => null,
+};
 
 export default Form;
--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,59 +1,62 @@
-import { useEffect, useState } from "react";
-import { useData } from "../../contexts/DataContext";
-import { getMonth } from "../../helpers/Date";
+import React, { useEffect, useState } from 'react';
+import { useData } from '../../contexts/DataContext';
+import { getMonth } from '../../helpers/Date';
 
-import "./style.scss";
+import './style.scss';
 
 const Slider = () => {
-  const { data } = useData();
-  const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
-  };
-  useEffect(() => {
-    nextCard();
-  });
-  return (
-    <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
-            </div>
-          </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
-      ))}
-    </div>
-  );
+	const { data } = useData();
+	const [index, setIndex] = useState(0);
+	const byDateDesc = data?.focus.sort((evtA, evtB) =>
+		new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
+	);
+	const nextCard = () => {
+		if (byDateDesc !== undefined) {
+			setTimeout(
+				() => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0), // index < byDateDesc.length - 1, if the index is at the last position it will reset to 0
+				5000
+			);
+		}
+	};
+	useEffect(() => {
+		nextCard();
+	});
+
+	return (
+		<div className="SlideCardList">
+			{byDateDesc?.map((event, idx) => (
+				<React.Fragment key={event.title}>
+					<div
+						className={`SlideCard SlideCard--${
+							index === idx ? 'display' : 'hide'
+						}`}
+					>
+						<img src={event.cover} alt="forum" />
+						<div className="SlideCard__descriptionContainer">
+							<div className="SlideCard__description">
+								<h3>{event.title}</h3>
+								<p>{event.description}</p>
+								<div>{getMonth(new Date(event.date))}</div>
+							</div>
+						</div>
+					</div>
+					<div className="SlideCard__paginationContainer">
+						<div className="SlideCard__pagination">
+							{byDateDesc.map((_, radioIdx) => (
+								<input
+									key={`${_.title}`}
+									type="radio"
+									name="radio-button"
+									checked={index === radioIdx} // use index state to sync the dots with the slider's display
+									readOnly
+								/>
+							))}
+						</div>
+					</div>
+				</React.Fragment>
+			))}
+		</div>
+	);
 };
 
 export default Slider;
